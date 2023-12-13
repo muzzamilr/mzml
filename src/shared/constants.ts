f0fb3877ac128fa6ecab640925c5b8261282a906
muzzamilr <mmuzzamil90@gmail.com>
2023-12-13 18:58:26 +0500

    update theme

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_TITLE = "Muzzamil Here!" as const;
+export const DEFAULT_TITLE = "Muzzamil Raza" as const;
 export const DEFAULT_DESC =
 	"Hi! This is a portfolio/introductory website for Muzzamil Raza" as const;
